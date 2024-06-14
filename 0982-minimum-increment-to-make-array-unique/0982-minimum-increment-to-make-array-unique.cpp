fd9948f4278e10ff63decc0bbab45674aa4f7a7e
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-06-14 10:04:39 +0530

    Time: 105 ms (88.58%), Space: 69.1 MB (79.13%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        // return 0;
-        vector<int>a(1000000,0);
-        int temp=*max_element(nums.begin(),nums.end());
-        long  ans=0;
-        int t=0,mod=1e9+7;
-        for(auto x:nums)a[x]++;
-        for(int i=0;i<=temp;i++){
-            if(a[i]>1){
-                t=max(t,i)+1;
-                while(a[i]>1){
-
-                    while(t<1000000){
-                        // cout<<t<<endl;
-                        if(a[t]==0){
-                            a[t]=1;
-                            // cout<<a[t-1];
-                            ans=(ans+t-i)%mod;
-                            break;
-                        }
-                        t++;
-                    }
-                    a[i]--;
-                }
-            }
+       sort(nums.begin(),nums.end());
+       long ans=0,t=0;
+       for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1]){
+            ans++;
+            nums[i]++;
+            t++;
         }
-        return ans;
+        if(nums[i]<nums[i-1]){
+            ans+=(nums[i-1]-nums[i])+1;
+            nums[i]=nums[i-1]+1;
+        }
+        
+       }
+       return ans;
     }
 };
