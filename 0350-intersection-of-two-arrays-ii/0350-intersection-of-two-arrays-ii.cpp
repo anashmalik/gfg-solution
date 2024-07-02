7f5bb1b7949931df70659cde14c69c3c6ed00c53
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-07-02 08:30:57 +0530

    Time: 4 ms (59.42%), Space: 14.3 MB (15.24%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>a(1001,0),b(1001,0);
-        for(auto x:nums1)a[x]++;
-        for(auto x:nums2)b[x]++;
-        nums1.clear();
-        for(int i=0;i<1001;i++){
-            int t=min(a[i],b[i]);
-            while(t--){
-                nums1.push_back(i);
-            }
+       unordered_map<int,int>mp;
+       for(auto const &x:nums1)mp[x]++;
+       nums1.clear();
+       for(auto const &x:nums2){
+        if(mp[x]>0){
+            nums1.push_back(x);
+            mp[x]--;
         }
-        return nums1;
+       }
+       return nums1;
     }
 };
