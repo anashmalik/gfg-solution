5057b07dfd8edda0742807f1d3c488b20f78049e
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-07-28 22:45:29 +0530

    Time: 423 ms (83.38%), Space: 182.5 MB (89.58%) - LeetHub

--- 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
@@ -1,10 +1,3 @@
-inline const auto optimize = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
-
 class Solution {
     static constexpr int howLong(int needed, int time, int change) {
         int ans = 0;
@@ -20,18 +13,18 @@ class Solution {
 public:
     static int secondMinimum(int N, const vector<vector<int>>& edges, int time,
                              int change) {
-        std::vector<std::vector<int>> G(N);
+         vector< vector<int>> G(N);
         for (const auto& edge : edges) {
             G[edge[0] - 1].push_back(edge[1] - 1);
             G[edge[1] - 1].push_back(edge[0] - 1);
         }
 
-        std::deque<int> Q;
-        std::vector<char> seen(N * 2);
+         deque<int> Q;
+         vector<char> seen(N * 2);
         Q.push_front(0);
         seen[0] = true;
 
-        int maxLength = std::numeric_limits<int>::max();
+        int maxLength =  numeric_limits<int>::max();
 
         int wave = 0;
         bool odd = true;
@@ -43,11 +36,11 @@ public:
                 Q.pop_front();
 
                 if (pos == N - 1) {
-                    if (maxLength == std::numeric_limits<int>::max()) {
+                    if (maxLength ==  numeric_limits<int>::max()) {
                         maxLength = wave + 2;
                         continue;
                     } else {
-                        maxLength = std::min(maxLength, wave);
+                        maxLength =  min(maxLength, wave);
                         return howLong(maxLength, time, change);
                     }
                 }
