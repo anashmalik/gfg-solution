c6973633a9c85325e3ca7a366a4d06a337e9303a
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-08-02 08:47:05 +0530

    Time: 62 ms (83.89%), Space: 82.8 MB (68.76%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,7 +3,7 @@ public:
     int minSwaps(vector<int>& nums) {
         int t=0,ans=INT_MAX,k=0,c=0,j=0;
         for(auto x:nums)if(x)t++;
-        for(int i=0;i<t-1;i++)nums.push_back(nums[i]);
+        
         for(int i=0;i<nums.size();i++){
             if(k<t){if(nums[i])c++;
             k++;}
@@ -13,7 +13,13 @@ public:
             }
             ans=min(ans,t-c);
         }
-       
+       for(int i=0;i<t-1;i++){
+            {
+                if(nums[i])c++;
+                if(nums[j++])c--;
+            }
+            ans=min(ans,t-c);
+       }
         return ans;
     }
 };
